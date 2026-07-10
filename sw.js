c8d471170a65df02a594bd3feb70a71cdfe70583
Aerotsunami <gradusteam@gmail.com>
2026-07-10 10:45:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tense-day-v1";
+const CACHE = "tense-day-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
