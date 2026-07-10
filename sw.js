237674d155600ce5c3f4c8483e8ab57c27eb15e0
Aerotsunami <gradusteam@gmail.com>
2026-07-10 11:08:42 +0200

    Refresh offline cache for recognition mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tense-day-v2";
+const CACHE = "tense-day-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -17,4 +17,3 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
-
