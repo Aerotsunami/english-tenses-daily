0bbf3c8c781c2d07c087f472bd94859625e682b3
Aerotsunami <gradusteam@gmail.com>
2026-08-18 22:53:35 +0200

    Add articles practice and compact topic catalog

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tense-day-v6";
+const CACHE = "tense-day-v7";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
