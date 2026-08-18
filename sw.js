5d4732250a21aa895f0bbb5374cdd952dd5b4686
Aerotsunami <gradusteam@gmail.com>
2026-08-18 23:19:01 +0200

    Expand helpers with modal sentence building

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tense-day-v9";
-const ASSETS = ["./", "./index.html", "./styles.css?v=9", "./app.js?v=9", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "tense-day-v10";
+const ASSETS = ["./", "./index.html", "./styles.css?v=10", "./app.js?v=10", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
