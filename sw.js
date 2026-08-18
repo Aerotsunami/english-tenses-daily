2b135daf6f883081cf9186f0269642d00d43c588
Aerotsunami <gradusteam@gmail.com>
2026-08-18 22:36:26 +0200

    Add speech patterns and tense practice

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tense-day-v4";
+const CACHE = "tense-day-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
