d5fda12b9709b33bd92b48a78ba11d1d34b66f27
Aerotsunami <gradusteam@gmail.com>
2026-08-19 18:42:09 +0200

    Refresh PWA cache for managed to practice

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tense-day-v13";
-const ASSETS = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "tense-day-v14";
+const ASSETS = ["./", "./index.html", "./styles.css?v=14", "./app.js?v=14", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
