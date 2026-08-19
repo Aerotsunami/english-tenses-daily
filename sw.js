275765c073882cea12d716eea0d07a98f073e1f1
Aerotsunami <gradusteam@gmail.com>
2026-08-19 18:35:25 +0200

    Refresh PWA cache after practice label fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tense-day-v12";
-const ASSETS = ["./", "./index.html", "./styles.css?v=12", "./app.js?v=12", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "tense-day-v13";
+const ASSETS = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
